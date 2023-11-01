4fc8540042aa9aec16bd85bf19ad7e35d31cb509
takudzwa-ngoma <ngomatakudzwa1@gmail.com>
2023-11-01 16:58:07 +0200

    added files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Nevbar from './componets/Nevbar';
+
+
+function App() {
+  return (
+    <div className="container">
+     <Nevbar/>
+      <div className="body">body</div>
+      <div className="footer">footer</div>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/componets/Nevbar.jsx
@@ -0,0 +1,26 @@
+function Nevbar(){
+    return(
+        <div className="Nevbar">
+
+        <div classname="log">log</div>
+        <div className="links">
+          <div className="linkItem">contact</div>
+          <div className="linkItem">explore</div>
+          <div className="linkItem">about</div>
+          </div>
+        <div className="signUp">signUp</div>
+      </div>
+    )
+}
+export default  Nevbar;
+//another way of creating a function in js
+// const nevbar=()=>{ <div className="Nevbar">
+
+// <div classname="log">log</div>
+// <div className="links">
+//   <div className="linkItem">contact</div>
+//   <div className="linkItem">explore</div>
+//   <div className="linkItem">about</div>
+//   </div>
+// <div className="signUp">signUp</div>
+// </div>} 
